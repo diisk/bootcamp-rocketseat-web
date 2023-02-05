29156d45a10941b3c0dcb79def6d1d08f80de884
diisk <diisk@hotmail.com>
2023-02-05 19:52:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './styles/global.css';
+
+
+import { Habit } from './components/Habit'
+
+function App() {
+  return (
+    <>
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+    </>
+  )
+}
+
+export default App
